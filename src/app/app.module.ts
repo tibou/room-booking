451e65c167269636815b4eaf65ed42ceb469184c
Tiburce SOTOHOU <tiburce.sotohou@adservio.fr>
2023-09-12 16:37:51 +0200

    Firebase integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Firebase
+import {AngularFireModule} from "@angular/fire/compat";
+import {AngularFireAuthModule} from "@angular/fire/compat/auth";
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+import {AngularFireStorageModule} from "@angular/fire/compat/storage";
+import {AngularFireDatabaseModule} from "@angular/fire/compat/database";
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyChVTJ7FDDJVQNph0NAaUyNaqeS6NiquSA",
+        authDomain: "room-booking-346c5.firebaseapp.com",
+        projectId: "room-booking-346c5",
+        storageBucket: "room-booking-346c5.appspot.com",
+        messagingSenderId: "1012071228567",
+        appId: "1:1012071228567:web:89c445ab807339eb7775d2",
+        measurementId: "G-6EZQ0XD9JQ"
+      }
+}
